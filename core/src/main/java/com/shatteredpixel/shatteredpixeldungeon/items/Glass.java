9d154a8a42146276e8e752172b2be7487401ab33
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 22:50:35 +0200

    Update Glass.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Glass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Glass.java
@@ -1,8 +1,11 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
 public class Glass extends Item {
-    { image = 254; } 
-    @Override public String name() { return "стекло"; }
-    @Override public String info() { return "Чистое закаленное стекло."; }
-    @Override public boolean isStackable() { return true; }
+    {
+        image = 254;
+        stackable = true;
+    }
+
+    @Override
+    public String name() { return "стекло"; }
 }
