8452d14d39dde1507055351a697045492223b41b
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 21:43:26 +0200

    Create SmallCrab.java

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SmallCrab.java
@@ -0,0 +1,77 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2026 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.CrabSprite;
+import com.watabou.utils.Random;
+
+public class SmallCrab extends Mob {
+
+	{
+		name = "малый краб";
+		spriteClass = CrabSprite.class;
+		
+		// Твои параметры: HP 9
+		HP = HT = 9;
+		
+		// Навык защиты 100 при низком уровне игрока даст ~80% промахов
+		defenseSkill = 100;
+
+		// Опыт, который ты просил
+		exp = 3;
+
+		maxLvl = 5;
+	}
+
+	@Override
+	public int damageRoll() {
+		// Твой урон: 1-5
+		return Random.NormalIntRange( 1, 5 );
+	}
+	
+	@Override
+	public int attackSkill( Char target ) {
+		// Чуть выше чем у крысы
+		return 10;
+	}
+	
+	@Override
+	public int drRoll() {
+		// Базовая броня краба
+		return Random.NormalIntRange(0, 2);
+	}
+
+	@Override
+	public void spawn( int pos ) {
+		super.spawn( pos );
+		// Делаем его визуально маленьким (70% от оригинала)
+		if (sprite != null) {
+			sprite.scale.set( 0.7f );
+		}
+	}
+
+	public static float spawnChance() {
+		return 0.2f; // 20%
+	}
+}
+
