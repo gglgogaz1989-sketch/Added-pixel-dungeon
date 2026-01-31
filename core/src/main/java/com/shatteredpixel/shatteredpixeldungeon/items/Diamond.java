afa021de24a6a51741cf3edaa5331da8616a97fa
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 23:10:58 +0200

    Update Diamond.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Diamond.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Diamond.java
@@ -1,22 +1,18 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfLiquidFlame;
+
 public class Diamond extends Item {
-    {
-        image = 255;
-        stackable = true;
-    }
-
-    @Override
-    public String name() { 
-        return "алмаз"; 
-    }
-
-    @Override
-    public String info() { 
-        return "Невероятно твердый и чистый драгоценный камень."; 
-    }
-
-    public int price() { 
-        return 300; 
+    { image = 255; stackable = true; }
+    @Override public String name() { return "алмаз"; }
+    
+    public static class Recipe extends Recipe.SimpleRecipe {
+        {
+            inputs = new Class[]{HardIron.class, Glass.class, PotionOfLiquidFlame.class};
+            inQuantity = new int[]{1, 1, 1};
+            output = Diamond.class;
+            outQuantity = 1;
+            cost = 10;
+        }
     }
 }
