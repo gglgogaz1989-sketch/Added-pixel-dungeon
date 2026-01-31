716a988d8d6deb01ac2408ba2e8342d07d6e3515
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 23:11:57 +0200

    Update Glass.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Glass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Glass.java
@@ -1,11 +1,18 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
-public class Glass extends Item {
-    {
-        image = 254;
-        stackable = true;
-    }
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfLiquidFlame;
 
-    @Override
-    public String name() { return "стекло"; }
+public class Glass extends Item {
+    { image = 254; stackable = true; }
+    @Override public String name() { return "стекло"; }
+
+    public static class Recipe extends Recipe.SimpleRecipe {
+        {
+            inputs = new Class[]{PotionOfLiquidFlame.class, Bottle.class};
+            inQuantity = new int[]{1, 1};
+            output = Glass.class;
+            outQuantity = 1;
+            cost = 2;
+        }
+    }
 }
