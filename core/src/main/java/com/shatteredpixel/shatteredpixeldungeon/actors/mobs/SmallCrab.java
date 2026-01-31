20c1f4c7dc681b81ad32a82e17ba44ab54f8f352
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 21:54:17 +0200

    Update SmallCrab.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SmallCrab.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SmallCrab.java
@@ -6,51 +6,48 @@ import com.watabou.utils.Random;
 
 public class SmallCrab extends Mob {
 
-    {
-        spriteClass = CrabSprite.class;
-        
-        HP = HT = 9;
-        defenseSkill = 100;
-        
-        // Попробуем прописать опыт напрямую в переменную, 
-        // так как метод exp() у тебя не переопределяется
-        exp = 3;
-    }
+	{
+		spriteClass = CrabSprite.class;
+		
+		// Твои параметры: HP 9
+		HP = HT = 9;
+		
+		// 80% уклонения за счет огромного навыка защиты
+		defenseSkill = 100;
+		
+		// В твоей версии опыт пишется большими буквами:
+		EXP = 3;
+		maxLvl = 5;
+	}
 
-    @Override
-    public String name() {
-        return "малый краб";
-    }
+	@Override
+	public String name() {
+		return "малый краб";
+	}
+	
+	@Override
+	public int damageRoll() {
+		// Твой урон 1-5
+		return Random.NormalIntRange( 1, 5 );
+	}
+	
+	@Override
+	public int attackSkill( Char target ) {
+		return 10;
+	}
+	
+	@Override
+	public int drRoll() {
+		// Базовая броня
+		return Random.NormalIntRange(0, 2);
+	}
 
-    @Override
-    public int drRoll() {
-        return Random.NormalIntRange(0, 2);
-    }
-
-    @Override
-    public int damageRoll() {
-        return Random.NormalIntRange( 1, 5 );
-    }
-
-    @Override
-    public int attackSkill( Char target ) {
-        return 10;
-    }
-
-    // Если переменная exp выше выдаст ошибку, используй этот способ без @Override
-    // Но пока оставляем как переменную.
-
-    @Override
-    public void spawn( int pos ) {
-        super.spawn( pos );
-        // Если scale не сработает, краб просто будет обычного размера, 
-        // но игра НЕ вылетит
-        if (sprite != null) {
-            sprite.scale.set( 0.7f );
-        }
-    }
-
-    public static float spawnChance() {
-        return 0.2f;
-    }
+	@Override
+	public void spawn( int pos ) {
+		super.spawn( pos );
+		// Уменьшаем размер до 70%
+		if (sprite != null) {
+			sprite.scale.set( 0.7f );
+		}
+	}
 }
