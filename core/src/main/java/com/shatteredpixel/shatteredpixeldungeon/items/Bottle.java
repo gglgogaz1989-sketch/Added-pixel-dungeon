e58a69a498a9ca6fae29e757896c1708784772d0
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 22:52:07 +0200

    Update Bottle.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bottle.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bottle.java
@@ -1,8 +1,11 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
 public class Bottle extends Item {
-    { image = 253; }
-    @Override public String name() { return "пустая бутылка"; }
-    @Override public String info() { return "Прозрачный сосуд для жидкостей."; }
-    @Override public boolean isStackable() { return true; }
+    {
+        image = 253;
+        stackable = true;
+    }
+
+    @Override
+    public String name() { return "пустая бутылка"; }
 }
