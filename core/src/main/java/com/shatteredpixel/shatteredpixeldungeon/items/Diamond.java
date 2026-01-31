91a5760a4519bbdd08cada0e17c2413858aa4422
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 22:55:47 +0200

    Update Diamond.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Diamond.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Diamond.java
@@ -2,12 +2,24 @@ package com.shatteredpixel.shatteredpixeldungeon.items;
 
 public class Diamond extends Item {
     {
-        image = 255;
+        // Тот самый голубой кристалл, который ты нарисовал последним
+        image = 255; 
         stackable = true;
     }
-    @Override
-    public String name() { return "алмаз"; }
-    @Override
-    public int price() { return 300; }
-}
 
+    @Override
+    public String name() {
+        return "алмаз";
+    }
+
+    @Override
+    public String info() {
+        return "Невероятно твердый и чистый драгоценный камень. " +
+               "Его грани светятся внутренним светом, способным указать путь.";
+    }
+
+    @Override
+    public int price() {
+        return 300; // Твоя цена в магазине
+    }
+}
