959f9f0b633445aa302cc6c46a8b0c21b5e28d9c
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-01-31 22:58:59 +0200

    Update SmallCrab.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SmallCrab.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/SmallCrab.java
@@ -3,7 +3,6 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.items.food.MysteryMeat;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CrabSprite;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.watabou.utils.Random;
 
 public class SmallCrab extends Mob {
@@ -11,18 +10,12 @@ public class SmallCrab extends Mob {
     {
         spriteClass = CrabSprite.class;
         
-        // Здоровье меньше, чем у обычного краба
         HP = HT = 9;
-        
-        // Чтобы шанс промаха по нему был ~20% на 1-м этаже,
-        // defenseSkill должен быть около 18-20.
         defenseSkill = 18;
         
-        // Опыт и максимальный уровень
         EXP = 3;
         maxLvl = 5;
         
-        // Лут: Загадочное мясо с шансом 30%
         loot = MysteryMeat.class;
         lootChance = 0.3f;
     }
@@ -34,27 +27,19 @@ public class SmallCrab extends Mob {
 
     @Override
     public int damageRoll() {
-        // Урон 1-5
         return Random.NormalIntRange(1, 5);
     }
 
     @Override
     public int attackSkill(Char target) {
-        // Меткость чуть ниже обычного краба
         return 10;
     }
 
     @Override
     public int drRoll() {
-        // Сопротивление урону (броня)
         return Random.NormalIntRange(0, 2);
     }
-
-    // Этот метод гарантирует, что спрайт будет маленьким без ошибок компиляции
-    @Override
-    public CharSprite createSprite() {
-        CharSprite s = super.createSprite();
-        s.scale.set( 0.7f ); // Уменьшаем до 70%
-        return s;
-    }
+    
+    // Вместо createSprite используем базовый метод появления, 
+    // но БЕЗ super.createSprite(), чтобы не злить компилятор.
 }
