d903eee2e0166b064bef4f218d2a409228b9e0d8
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 11:43:15 +0200

    Update ObsidianChest.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/features/ObsidianChest.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/features/ObsidianChest.java
@@ -4,30 +4,27 @@ import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.items.keys.ObsidianKey;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Saber;
+import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.utils.GLog;
 
-public class ObsidianChest extends Feature {
+// Если Feature не находится, попробуй заменить на Level.Feature или просто убрать extends, если это тайл
+public class ObsidianChest { 
     
-    // Это метод, который срабатывает, когда игрок нажимает на сундук
-    @Override
-    public void execute( Hero hero, int pos ) {
-        // Проверяем, есть ли у героя обсидиановый ключ
-        ObsidianKey key = hero.belongsTo.getItem( ObsidianKey.class );
+    public static void open( Hero hero, int pos ) {
+        // Проверка ключа через инвентарь (в Shattered это обычно hero.belongings.getItem)
+        ObsidianKey key = hero.belongings.getItem( ObsidianKey.class );
         
         if (key != null) {
-            key.detach( hero.belongsTo ); // Тратим ключ
+            key.detach( hero.belongings ); 
             
-            // "Ломаем" сундук (превращаем клетку в обычный пол)
             Dungeon.level.set( pos, Terrain.EMPTY_SP ); 
-            Dungeon.level.updateVisuals( pos );
+            // В новых версиях вместо updateVisuals используется:
+            Dungeon.observe(); 
             
-            // Выбрасываем награду (Саблю)
             Dungeon.level.drop( new Saber(), pos ).sprite.drop();
-            
             GLog.p( "Обсидиановый сундук открыт!" );
         } else {
             GLog.w( "Этот сундук заперт на обсидиановый замок." );
         }
     }
 }
-
