dd958b1006ad94b4ec8e5a8dfe541f681810cd94
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 13:28:47 +0200

    Update ObsidianKey.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/ObsidianKey.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/ObsidianKey.java
@@ -1,24 +1,23 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
-import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
-
 public class ObsidianKey extends Item {
-
     {
-        name = "Обсидиановый ключ";
-        image = 58; // Твой индекс, который мы посчитали (Ряд 3, Колонка 10)
-        stackable = true; // Можно носить несколько в одной ячейке
+        // Имя в коде задается так, если нет ресурсов:
+        image = 58; 
     }
 
     @Override
-    public String desc() {
-        return "Этот тяжелый ключ высечен из вулканического стекла. " +
-               "Он кажется слишком хрупким для обычных дверей, но идеально подходит для обсидиановых замков.";
+    public String Name() {
+        return "Обсидиановый ключ";
     }
-    
+
+    @Override
+    public String info() {
+        return "Этот тяжелый ключ высечен из вулканического стекла. Идеально подходит для обсидиановых замков.";
+    }
+
     @Override
     public int price() {
         return 100;
     }
 }
-
