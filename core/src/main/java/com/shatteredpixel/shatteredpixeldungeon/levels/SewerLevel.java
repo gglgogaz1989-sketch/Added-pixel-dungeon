54cc51c4c9eab8f1adfb30ecbc1309cc6fa029b4
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 14:27:27 +0200

    Update SewerLevel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/SewerLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/SewerLevel.java
@@ -1,24 +1,3 @@
- /*
- * Pixel Dungeon
- * Copyright (C) 2012-2015 Oleg Dolya
- *
- * Shattered Pixel Dungeon
- * Copyright (C) 2014-2025 Evan Debenham
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
-
 package com.shatteredpixel.shatteredpixeldungeon.levels;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
@@ -34,17 +13,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.Amulet;
 import com.shatteredpixel.shatteredpixeldungeon.levels.features.LevelTransition;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.SewerPainter;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.AlarmTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.ChillingTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.ConfusionTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.FlockTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.GatewayTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.OozeTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.ShockingTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.SummoningTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.TeleportationTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.ToxicTrap;
-import com.shatteredpixel.shatteredpixeldungeon.levels.traps.WornDartTrap;
+import com.shatteredpixel.shatteredpixeldungeon.levels.traps.*;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.SurfaceScene;
@@ -60,6 +29,8 @@ import com.watabou.utils.Callback;
 import com.watabou.utils.ColorMath;
 import com.watabou.utils.PointF;
 import com.watabou.utils.Random;
+
+import java.util.ArrayList;
 
 public class SewerLevel extends RegularLevel {
 
@@ -84,56 +55,32 @@ public class SewerLevel extends RegularLevel {
 			Music.INSTANCE.playTracks(SEWER_TRACK_LIST, SEWER_TRACK_CHANCES, false);
 		}
 	}
-	
+
 	@Override
 	protected int specialRooms(boolean forceMax) {
 		int n = super.specialRooms(forceMax);
-		// Добавляем место под 1 комнату на 1-м этаже
+		// Добавляем место под 1 обсидиановую комнату только на 1-м этаже
 		return Dungeon.depth == 1 ? n + 1 : n;
 	}
 
 	@Override
-	protected void addSpecialRooms(java.util.ArrayList<com.shatteredpixel.shatteredpixeldungeon.levels.rooms.Room> rooms) {
+	protected void addSpecialRooms(ArrayList<com.shatteredpixel.shatteredpixeldungeon.levels.rooms.Room> rooms) {
 		super.addSpecialRooms(rooms);
 		if (Dungeon.depth == 1) {
-			rooms.add(new com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.ObsidianRoom());
+			rooms.add(new ObsidianRoom());
 		}
-	}
-	
-	}
-	
-	@Override
-	protected int specialRooms(boolean forceMax) {
-		if (forceMax) return 2;
-		//1 to 2, average 1.8
-		return 1+Random.chances(new float[]{1, 4});
-	}
-		@Override
-	protected int specialRooms(boolean forceMax) {
-		int n = super.specialRooms(forceMax);
-		if (Dungeon.depth == 1) n++; 
-		return n;
 	}
 
-	@Override
-	public void addSpecialRooms(java.util.ArrayList<com.shatteredpixel.shatteredpixeldungeon.levels.rooms.Room> rooms) {
-		// Мы не вызываем super, если его нет в родителе
-		if (Dungeon.depth == 1) {
-			rooms.add(new com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard.ObsidianRoom());
-		}
-	}
-	
-	
 	@Override
 	public String tilesTex() {
 		return Assets.Environment.TILES_SEWERS;
 	}
-	
+
 	@Override
 	public String waterTex() {
 		return Assets.Environment.WATER_SEWERS;
 	}
-	
+
 	@Override
 	protected Class<?>[] trapClasses() {
 		return Dungeon.depth == 1 ?
@@ -142,7 +89,7 @@ public class SewerLevel extends RegularLevel {
 						ChillingTrap.class, ShockingTrap.class, ToxicTrap.class, WornDartTrap.class,
 						AlarmTrap.class, OozeTrap.class,
 						ConfusionTrap.class, FlockTrap.class, SummoningTrap.class, TeleportationTrap.class, GatewayTrap.class };
-}
+	}
 
 	@Override
 	protected float[] trapChances() {
@@ -159,7 +106,7 @@ public class SewerLevel extends RegularLevel {
 		Ghost.Quest.spawn( this, roomExit );
 		super.createMobs();
 	}
-	
+
 	@Override
 	public boolean activateTransition(Hero hero, LevelTransition transition) {
 		if (transition.type == LevelTransition.Type.SURFACE){
@@ -175,10 +122,7 @@ public class SewerLevel extends RegularLevel {
 				Statistics.ascended = true;
 				Game.switchScene(SurfaceScene.class, new Game.SceneChangeCallback() {
 					@Override
-					public void beforeCreate() {
-
-					}
-
+					public void beforeCreate() {}
 					@Override
 					public void afterCreate() {
 						Badges.validateHappyEnd();
@@ -213,7 +157,6 @@ public class SewerLevel extends RegularLevel {
 
 	@Override
 	public void destroy(int pos) {
-		//if we're burning  sewers barrels
 		int terr = map[pos];
 		if (terr == Terrain.REGION_DECO){
 			set(pos, Terrain.WATER);
@@ -232,7 +175,7 @@ public class SewerLevel extends RegularLevel {
 			}
 		}
 	}
-	
+
 	@Override
 	public String tileName( int tile ) {
 		switch (tile) {
@@ -245,7 +188,7 @@ public class SewerLevel extends RegularLevel {
 				return super.tileName( tile );
 		}
 	}
-	
+
 	@Override
 	public String tileDesc(int tile) {
 		switch (tile) {
@@ -260,38 +203,30 @@ public class SewerLevel extends RegularLevel {
 				return super.tileDesc( tile );
 		}
 	}
-	
+
 	private static class Sink extends Emitter {
-		
 		private int pos;
 		private float rippleDelay = 0;
-		
 		private static final Emitter.Factory factory = new Factory() {
-			
 			@Override
 			public void emit( Emitter emitter, int index, float x, float y ) {
 				WaterParticle p = (WaterParticle)emitter.recycle( WaterParticle.class );
 				p.reset( x, y );
 			}
 		};
-		
+
 		public Sink( int pos ) {
 			super();
-			
 			this.pos = pos;
-			
 			PointF p = DungeonTilemap.tileCenterToWorld( pos );
 			pos( p.x - 2, p.y + 3, 4, 0 );
-			
 			pour( factory, 0.1f );
 		}
-		
+
 		@Override
 		public void update() {
 			if (visible = (pos < Dungeon.level.heroFOV.length && Dungeon.level.heroFOV[pos])) {
-				
 				super.update();
-				
 				if (!isFrozen() && (rippleDelay -= Game.elapsed) <= 0) {
 					Ripple ripple = GameScene.ripple( pos + Dungeon.level.width() );
 					if (ripple != null) {
@@ -302,28 +237,21 @@ public class SewerLevel extends RegularLevel {
 			}
 		}
 	}
-	
+
 	public static final class WaterParticle extends PixelParticle {
-		
 		public WaterParticle() {
 			super();
-			
 			acc.y = 50;
 			am = 0.5f;
-			
 			color( ColorMath.random( 0xb6ccc2, 0x3b6653 ) );
 			size( 2 );
 		}
-		
 		public void reset( float x, float y ) {
 			revive();
-			
 			this.x = x;
 			this.y = y;
-			
 			speed.set( Random.Float( -2, +2 ), 0 );
-			
 			left = lifespan = 0.4f;
 		}
 	}
-}
+	}
